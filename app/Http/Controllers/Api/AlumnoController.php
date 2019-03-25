d13c5572924e350c06d5cd509d556b962873c899
igomis <ignasi.gomis.mullor@gmail.com>
2019-03-25 16:25:57 +0100

    alumnos

--- app/Http/Controllers/Api/AlumnoController.php
+++ app/Http/Controllers/Api/AlumnoController.php
@@ -8,29 +8,16 @@ use App\Entities\Alumne;
 
 class AlumnoController extends ApiBaseController
 {
-
     use traitRelation;
 
     public function model(){
         return 'Alumno';
     }
-
-
-    protected function relationShip()
+   protected function relationShip()
     {
         return 'ciclos';
     }
 
-    protected function validateRelation($relations)
-    {
-        foreach ($relations as $index => $related){
-            $array[$index]['validado'] = 0;
-            foreach ($related as $key => $value)
-                if ($key != 'validado') $array[$index][$key] = $value;
-        }
-        return $array;
-
-    }
 }
 
 
