a857cb717b6375342b8df1b92785bea6520290b3
juanseguravasco <jsegura@cipfpbatoi.es>
2019-04-05 13:06:04 +0200

    cambios

--- resources/js/lib/API.js
+++ resources/js/lib/API.js
@@ -1,4 +1,5 @@
-const API_URL = 'https://borsaTreball.my/api/';
+const API_URL = 'https://borsaTreball.cipfpbatoi.es/api/';
+//const API_URL = 'http://borsaTreball.my/api/';
 
 import axios from 'axios'
 
