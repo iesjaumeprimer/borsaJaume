568d636657e7963cc86af6b444bfbc7680d69933
igomis <ignasi.gomis.mullor@gmail.com>
2019-04-07 16:57:56 +0200

    manageResponse

---
+++ database/migrations/2019_03_25_175046_alter_create_password_resets_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterCreatePasswordResetsTable extends Migration
+{
+    public function up()
+    {
+        Schema::table('password_resets', function (Blueprint $table){
+            $table->increments('id')->first();
+            $table->timestamp('updated_at')->nullable();
+        });
+    }
+    public function down()
+    {
+        Schema::table('password_resets', function (Blueprint $table) {
+            $table->dropColumn('id');
+            $table->dropColumn('updated_at');
+        });
+    }
+}
