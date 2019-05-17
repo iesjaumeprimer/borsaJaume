d8d3997c93e06b7aa5d5eb13a72ea792a2416874
igomis <ignasi.gomis.mullor@gmail.com>
2019-05-17 12:01:48 +0200

    manageResponse

--- app/Http/Controllers/Api/OfertaController.php
+++ app/Http/Controllers/Api/OfertaController.php
@@ -32,7 +32,7 @@ class OfertaController extends ApiBaseController
 
     private function filterIndex($archivada){
         if (AuthUser()->isEmpresa())
-            return OfertaResource::collection(Oferta::BelongsToEnterprise(AuthUser()->id)->where('archivada',$archivada));
+            return OfertaResource::collection(Oferta::BelongsToEnterprise(AuthUser()->id)->where('archivada',$archivada)->get());
         if (AuthUser()->isAlumno()){
             $ofertasFinalitzat = OfertaResource::collection(Oferta::BelongsToCicles(Alumno::find(AuthUser()->id)->ciclos->where('pivot.validado','=',true)->where('pivot.any', '!=', null))
                 ->where('validada',true)->where('activa',true)->where('estudiando',false)->where('archivada',false));
