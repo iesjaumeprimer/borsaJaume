71f65595513bf9cd2c3ef2bf9b4e330aac1eafb1
juanseguravasco <jsegura@cipfpbatoi.es>
2020-03-13 17:10:45 +0100

    cambios API

--- app/Http/Controllers/Api/MenuController.php
+++ app/Http/Controllers/Api/MenuController.php
@@ -11,8 +11,8 @@ class MenuController extends ApiBaseController
     }
 
     public function index(){
+        if (isset(AuthUser()->id)) return $this->resource::collection($this->entity::isRol(AuthUser()->rol)->orderBy('order')->get());
         return $this->resource::collection($this->entity::where('rol',9999)->orderBy('order')->get());
-        return $this->resource::collection($this->entity::isRol(AuthUser()->rol)->orderBy('order')->get());
     }
 
 }
