f3bddc9885abb8fef5b90aa435bcce3604ba0837
vagrant <vagrant@homestead>
2020-03-16 06:41:40 +0000

    actualitzacio

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,10 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if ($this->app->environment() !== 'production') {
-            $this->app->register(\Way\Generators\GeneratorsServiceProvider::class);
-            $this->app->register(\Xethron\MigrationsGenerator\MigrationsGeneratorServiceProvider::class);
-        }
+        //
     }
 
     /**
